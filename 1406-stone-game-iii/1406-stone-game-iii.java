e0344f4812bdd3e5a2771f13a863f35445bbb9dd
daxi-14 <105547951+daxi-14@users.noreply.github.com>
2026-08-03 17:36:17 +0530

    Time: 10 ms (83.24%), Space: 85.1 MB (88.92%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+
+        int[] dp = new int[n + 1];
+
+        // dp[n] = 0 automatically
+
+        for (int i = n - 1; i >= 0; i--) {
+            int take = 0;
+            dp[i] = Integer.MIN_VALUE;
+
+            for (int k = 0; k < 3 && i + k < n; k++) {
+                take += stoneValue[i + k];
+                dp[i] = Math.max(dp[i], take - dp[i + k + 1]);
+            }
+        }
+
+        if (dp[0] > 0)
+            return "Alice";
+        else if (dp[0] < 0)
+            return "Bob";
+        else
+            return "Tie";
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
