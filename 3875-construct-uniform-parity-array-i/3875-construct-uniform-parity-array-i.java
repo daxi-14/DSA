93cf66f2699160b4a2504c5f7e9a5ec91ca9e78c
daxi-14 <105547951+daxi-14@users.noreply.github.com>
2026-09-02 12:06:33 +0530

    Time: 0 ms (100%), Space: 44.9 MB (94.09%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+        /*int n = nums1.length;
+        int odd = 0, even = 0;
+        for (int num : nums1) {
+            if (num % 2 == 0) even++;
+            else odd++;
+        }
+        if (odd == n || even == n) return true;*/
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
