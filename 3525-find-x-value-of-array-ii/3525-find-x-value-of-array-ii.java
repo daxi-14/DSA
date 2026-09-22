ff4e633d5271b818b31031683c6d075b68b0d44e
daxi-14 <105547951+daxi-14@users.noreply.github.com>
2026-09-22 11:19:40 +0530

    Time: 209 ms (89.47%), Space: 285.1 MB (26.32%) - LeetHub - Time: O(n * k + q * k * log n), Space: O(n * k)

---
+++ 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.java
@@ -0,0 +1,123 @@
+class Solution {
+
+    class Node {
+        int prod;
+        int[] cnt;
+
+        Node(int k) {
+            cnt = new int[k];
+        }
+    }
+
+    int k;
+    Node[] tree;
+
+    private Node merge(Node a, Node b) {
+        Node res = new Node(k);
+
+        res.prod = (a.prod * b.prod) % k;
+
+        for (int i = 0; i < k; i++) {
+            res.cnt[i] = a.cnt[i];
+        }
+
+        for (int i = 0; i < k; i++) {
+            int rem = (a.prod * i) % k;
+            res.cnt[rem] += b.cnt[i];
+        }
+
+        return res;
+    }
+
+    private void build(int node, int l, int r, int[] nums) {
+        if (l == r) {
+            tree[node] = new Node(k);
+
+            int rem = nums[l] % k;
+            tree[node].prod = rem;
+            tree[node].cnt[rem] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid, nums);
+        build(node * 2 + 1, mid + 1, r, nums);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    private void update(int node, int l, int r, int index, int value) {
+        if (l == r) {
+            int rem = value % k;
+
+            tree[node] = new Node(k);
+            tree[node].prod = rem;
+            tree[node].cnt[rem] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (index <= mid) {
+            update(node * 2, l, mid, index, value);
+        } else {
+            update(node * 2 + 1, mid + 1, r, index, value);
+        }
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    private Node query(int node, int l, int r, int ql, int qr) {
+        if (ql <= l && r <= qr) {
+            return tree[node];
+        }
+
+        int mid = (l + r) / 2;
+
+        if (qr <= mid) {
+            return query(node * 2, l, mid, ql, qr);
+        }
+
+        if (ql > mid) {
+            return query(node * 2 + 1, mid + 1, r, ql, qr);
+        }
+
+        Node left = query(node * 2, l, mid, ql, qr);
+        Node right = query(node * 2 + 1, mid + 1, r, ql, qr);
+
+        return merge(left, right);
+    }
+
+    public int[] resultArray(int[] nums, int k, int[][] queries) {
+        this.k = k;
+
+        int n = nums.length;
+        tree = new Node[4 * n];
+
+        build(1, 0, n - 1, nums);
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int index = queries[i][0];
+            int value = queries[i][1];
+            int start = queries[i][2];
+            int x = queries[i][3];
+
+            update(1, 0, n - 1, index, value);
+
+            Node res = query(1, 0, n - 1, start, n - 1);
+
+            ans[i] = res.cnt[x];
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
